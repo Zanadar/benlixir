b0cc42e9263a8577dd78957464701847198a76a2
Zander Mackie <zmackie@gmail.com>
2016-09-30 07:11:10 -0400

    Metatdata for Hex

--- mix.exs
+++ mix.exs
@@ -3,30 +3,33 @@ defmodule Benlixir.Mixfile do
 
   def project do
     [app: :benlixir,
-     version: "0.0.1",
+     version: "0.1.7",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    A small bencode library.
+    """
+  end
+
+  defp package do
+    [name: :bendy,
+    maintainers: ["Zander Mackie"],
+    licenses: ["MIT"],
+    links: %{"Github" => "https://github.com/Zanadar/benlixir"}]
+  end
+
   defp deps do
-    []
+    [{:ex_doc, "~> 0.10", only: :dev}]
   end
 end
